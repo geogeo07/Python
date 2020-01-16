0f9ea122518bd20ce7d09b4b2d39be8e877a1a72
geovanni alvarez rivera <geovannitito9710@gmail.com>
2020-01-15 22:47:03 -0600

    El menu principal ya casi funciona por completo

--- test.py
+++ test.py
@@ -3,17 +3,17 @@ from tkinter import ttk
 from tkinter import messagebox
 import os
 
+from tkinter import filedialog
+
 import socket
 from urllib.request import urlopen
 
-#from PIL import ImageTk, Image
+
 
 loginWindow=Tk()
 mainMenu=Tk()
 
-#imagen1 = ImageTk.PhotoImage(Image.open("chavito.jpg"))
-#label1 = Label(loginWindow, image=imagen1)
-#label1.grid(row=1,column=1)
+
 
 
 
@@ -40,52 +40,16 @@ def cerrar_splashscreen():
 def progress(currentValue): #Llama
     progressbar["value"]=currentValue
 
-def validarContraseña(abrirVentana, usuarioLogin, contraLogin): #Valida la contra del login, cierra el login y abre el menu principal
-    if usuarioLogin == '1' and contraLogin == '1':
-       {
-           abrirVentana.deiconify()
-                    
-       } 
-    else:
-        messagebox.showwarning("Cuidado", "Password incorrecto")
 
-
-
-
-def hayInternet(): #Comprueba que exista alguna conexion a internet.
-   try:
-        response = urlopen('https://www.google.com/', timeout=10)
-        return True
-   except: 
-        return False
-
-def VentanaInternet ():
-    internetWindow = Toplevel(mainMenu)
-    internetWindow.title("Internet Window")
-    internetWindow.geometry('380x380')
-    internetWindow.configure(background='dark gray')
-   
-    internetWindow.resizable(0,0)
-    e1=Label(internetWindow, text="Password:" , bg="pink", fg= "white")
-    e1.pack(padx=5, pady=5, ipadx=5, ipady=5)
-    entrada1=Entry(internetWindow)
-    entrada1.pack(fill=X,padx=5,pady=58,ipadx=5, ipady=5)
-    if hayInternet == False: #Comprueba si hay internet.
-        ButtonInternet=Button(internetWindow,text="Por internet", fg="blue", state = 'disable' )
-    else:
-        ButtonInternet=Button(internetWindow,text="Por internet", fg="blue", state = 'normal' )
-
-    ButtonInternet.pack(side=TOP)
-    ButtonLocal=Button(internetWindow,text="Local", fg="blue")
-    ButtonLocal.pack(side=BOTTOM)
 
 def registroUsuario(username, contra):
     usuario_info = username
     contra_info = contra
-    file = open("usuario_info.txt", "w")
+    file = open("usuario_info.txt", "a")
     file.write(usuario_info + "\n")
-    file.write(contra_info)
+    file.write(contra_info + "\n")
     file.close()
+    messagebox.showinfo("Listo","Quedaste registrado")
 
 
 
@@ -99,11 +63,8 @@ def verificarUsuario(ventana, username, contra):
         
     else:
         print(usuario_info, contra_info)
-
         lista_archivos = os.listdir()
-    #print(lista_archivos, sep=' ', end='\n', file=sys.stdout, flush=False) Imprimir lista de archivos.
-
-   
+      
         archivo = open("usuario_info.txt", "r")
         verificar = archivo.read().splitlines()
         print(verificar)
@@ -122,53 +83,140 @@ def verificarUsuario(ventana, username, contra):
             messagebox.showwarning("Cuidado", "Usuario incorrecto")
          
 
+
+
+
+def hayInternet(): #Comprueba que exista alguna conexion a internet.
+   try:
+        response = urlopen('https://www.google.com/', timeout=10)
+        return True
+   except: 
+        return False
+
+
+def llenarInicio(ruta):
+    if ruta == '':
+        messagebox.showwarning("Cuidado", "no seleccionaste nada bro :( ")
+    else:
+        archivo = open(ruta, "r")
+        datos = archivo.read().splitlines()
+
+        #Primera linea: Nombre
+        posicion = datos.index("Nombre:")
+        nombre = datos[posicion+1] 
+
+        #Primera linea: Diametro
+        posicion = datos.index("Diametro:")
+        diametro = datos[posicion+1] 
+
+        #Segunda linea: Espesor
+        posicion = datos.index("Espesor:")
+        espesor = datos[posicion+1]
+
+        #Segunda linea: Espesor
+        posicion = datos.index("Largo:")
+        largo = datos[posicion+1]
+
+        print(diametro, espesor, largo)
+
+        TxtNombre.insert(0,nombre)
+        TxtDiametro.insert(0,diametro)
+        TxtEspesor.insert(0,espesor)
+        TxtLargo.insert(0,largo) 
     
-        
-        
+
+
+def abrirArchivo():
+    ruta=filedialog.askopenfilename(initialdir="/Proyecto de Titulacion (Python)", title ="Seleccione archivo", filetypes=(("txt files", "*.txt"), ("all files","*.*")))
+    print(ruta)
+    llenarInicio(ruta)
+
+def guardarArchivo():
+
+    if TxtNombre.get() == '' or TxtDiametro.get() == '' or TxtEspesor.get() == '' or TxtLargo.get() == '':
+        messagebox.showwarning("Cuidado","Revisa tus datos bro")
+    else:
+        lista_archivos = os.listdir()
+        if TxtNombre.get() + ".txt" in lista_archivos:
+            messagebox.showwarning("Cuidado", "Ese ya esta guardado bro")
+        else:
+            archivoNuevo = open(TxtNombre.get() + ".txt","w")
+            archivoNuevo.write("Nombre:" + "\n")
+            archivoNuevo.write(TxtNombre.get() + "\n")
+            archivoNuevo.write("Diametro:" + "\n")
+            archivoNuevo.write(TxtDiametro.get() + "\n")
+            archivoNuevo.write("Espesor:" + "\n")
+            archivoNuevo.write(TxtEspesor.get() + "\n")
+            archivoNuevo.write("Largo:" + "\n")
+            archivoNuevo.write(TxtLargo.get())
+            archivoNuevo.close()
+            messagebox.showinfo("Ya quedo", "Ya se guardo puñetas")
+
+
+ #######################
+ #crea y llama a las ventanas secundarias(INICIO).       
+
+def abrirVentanaInternet ():
+    ventanaInternet = Toplevel(mainMenu)
+    ventanaInternet.title("Internet Window")
+    ventanaInternet.geometry('380x380')
+    ventanaInternet.configure(background='dark gray')
    
-    
+    ventanaInternet.resizable(0,0)
+    e1=Label(ventanaInternet, text="Password:" , bg="pink", fg= "white")
+    e1.pack(padx=5, pady=5, ipadx=5, ipady=5)
+    entrada1=Entry(ventanaInternet)
+    entrada1.pack(fill=X,padx=5,pady=58,ipadx=5, ipady=5)
+    if hayInternet == False: #Comprueba si hay internet.
+        ButtonInternet=Button(ventanaInternet,text="Por internet", fg="blue", state = 'disable' )
+    else:
+        ButtonInternet=Button(ventanaInternet,text="Por internet", fg="blue", state = 'normal' )
+
+    ButtonInternet.pack(side=TOP)
+    ButtonLocal=Button(ventanaInternet,text="Local", fg="blue")
+    ButtonLocal.pack(side=BOTTOM)
 
 
-
-
-def VentanaAgregarUsuario ():
+def abrirVentanaAbrirCodigo():
     #Vetana de agregar usuario
-    addUserWindow = Toplevel(mainMenu)
+    ventanaAbrirCodigo = Toplevel(mainMenu)
     
-    addUserWindow.title("Agregar usuario")
-    addUserWindow.geometry('380x380')
-    addUserWindow.configure(background='dark gray')
+    ventanaAbrirCodigo.title("Abrir Codigo")
+    ventanaAbrirCodigo.geometry('380x380')
+    ventanaAbrirCodigo.configure(background='dark gray')
 
-    addUserWindow.resizable(0,0)
+    ventanaAbrirCodigo.resizable(0,0)
     #
-    EtiquetaUsuario=Label(addUserWindow, text="Usuario:" , bg="pink", fg= "white")
+
+
+
+def abrirVentanaAgregarUsuario():
+    #Vetana de agregar usuario
+    ventanaAgregarUsuario = Toplevel(mainMenu)
+    
+    ventanaAgregarUsuario.title("Agregar usuario")
+    ventanaAgregarUsuario.geometry('380x380')
+    ventanaAgregarUsuario.configure(background='dark gray')
+
+    ventanaAgregarUsuario.resizable(0,0)
+    #
+    EtiquetaUsuario=Label(ventanaAgregarUsuario, text="Usuario:" , bg="pink", fg= "white")
     EtiquetaUsuario.pack(padx=5, pady=5, ipadx=5, ipady=5)
-    TxtUser=Entry(addUserWindow)
+    TxtUser=Entry(ventanaAgregarUsuario)
     TxtUser.pack(fill=X,padx=5,pady=5,ipadx=5, ipady=5)
 
-    EtiquetaContra=Label(addUserWindow, text="Contraseña:", bg="pink", fg= "white")
+    EtiquetaContra=Label(ventanaAgregarUsuario, text="Contraseña:", bg="pink", fg= "white")
     EtiquetaContra.pack(padx=5, pady=5, ipadx=5, ipady=5)
-    TxtPass=Entry(addUserWindow)
+    TxtPass=Entry(ventanaAgregarUsuario)
     TxtPass.pack(fill=X,padx=5,pady=5,ipadx=5, ipady=5)
-    ButtonRegistrar=Button(addUserWindow,text="Registrar", fg="blue", state = 'normal' , command=lambda:registroUsuario(TxtUser.get(), TxtPass.get()))
+    ButtonRegistrar=Button(ventanaAgregarUsuario,text="Registrar", fg="blue", state = 'normal' , command=lambda:registroUsuario(TxtUser.get(), TxtPass.get()))
     
     ButtonRegistrar.pack(side=BOTTOM)
     #Fin ventana agregar usuario
-    
-    
+
+#crea y llama a las ventanas secundarias(FIN).
+#######################
    
-
-
-
-
-
-
-
-
-    
-
-
-
 
 #Creacion del login
 loginWindow.title("Login")
@@ -194,6 +242,7 @@ ButtonVerificar.pack(side=BOTTOM)
 
 #Crea una ventana hija del loginWindow
 splashScreen=Toplevel(loginWindow)
+splashScreen.title("Splash Screen")
 splashScreen.geometry("400x200")
 splashScreen.config(bg="black")
 splashScreen.protocol("WM_DELETE_WINDOW", "onexit")
@@ -208,9 +257,10 @@ ocultar(mainMenu)
 splashScreen.after(4000,cerrar_splashscreen)
 
 #Agregamos una etiqueta al splash.
-Label(splashScreen,text="BIENVENIDO A NUESTRA APLICACIÓN",bg="black",fg="white",font=(15)).pack()
+#Label(splashScreen,text="BIENVENIDO A NUESTRA APLICACIÓN",bg="black",fg="white",font=(15)).pack()
 
-
+imagen=PhotoImage(file="chavito.gif")
+foto=Label(splashScreen, image = imagen,).pack(side=TOP)
 #ProgressBar started
 maxValue=100
 progressbar=ttk.Progressbar(splashScreen,orient="horizontal",length=300,mode="determinate")
@@ -229,34 +279,78 @@ for i in range(divisions):
 
 
 #Creacion del menu
-mainMenu.focus_set()
-menu1 = Menu(mainMenu)
-mainMenu.geometry('380x380')
-mainMenu.config(menu=menu1)
+menuPrincipal = Menu(mainMenu)
+mainMenu.title("Main menu")
+mainMenu.geometry('380x480')
+mainMenu.config(menu=menuPrincipal)
+
+
+menu1 = Menu(menuPrincipal, tearoff=0)
+menuPrincipal.add_cascade(label="File", menu=menu1)
+
+
 menu1_1 = Menu(menu1, tearoff=0)
-menu1.add_cascade(label="Conexion", menu=menu1_1)
-menu1_1_1 = Menu(menu1_1, tearoff=0)
-menu1_1.add_cascade(label="Ver conexiones", menu=menu1_1_1)
-menu1_1_1.add_command(label="Arbrir Conexiones",command=lambda: imprimir("Guardar todo"))
-if hayInternet == False:
-    menu1_1_1.add_command(label="Internet", state = 'Disable')
-else:
-    menu1_1_1.add_command(label="Internet", command=VentanaInternet)
+menu1.add_cascade(label="Archivo nuevo", menu=menu1_1)
+menu1_1.add_command(label="Ingresar valores")
+menu1_1.add_command(label="Limpiar")
 
 menu1_2 = Menu(menu1, tearoff=0)
 menu1.add_cascade(label="Abrir", menu=menu1_2)
-menu1_2.add_command(label="Abrir codigo",command=lambda: imprimir("Abrir codigo"))
-menu1_2.add_separator()
-menu1_2_1 = Menu(menu1_2, tearoff=0)
-menu1_2.add_cascade(label="Abrir otra cosa", menu=menu1_2_1)
-menu1_2_1.add_command(label="ARchivo",command=lambda: imprimir("ARchivo"))
-menu1_2_1.add_command(label="Carpeta",command=lambda: imprimir("Carpeta") )
+menu1_2.add_command(label="Abrir codigo",command=abrirArchivo)
 
 
-menu1_3 = Menu(menu1, tearoff=0)
-menu1.add_cascade(label="Agregar Usuario", menu=menu1_3)
-menu1_3.add_command(label="Agregar Usuario",command=lambda:VentanaAgregarUsuario())
 
+
+
+menu2_1 = Menu(menuPrincipal, tearoff=0)
+
+menuPrincipal.add_cascade(label="Ventana manual", menu=menu2_1)
+menu2_1.add_command(label="Valores manuales")
+
+
+menu3_1 = Menu(menuPrincipal, tearoff=0)
+
+menuPrincipal.add_cascade(label="Agregar Usuario", menu=menu3_1)
+menu3_1.add_command(label="Agregar nuevo Usuario", command = abrirVentanaAgregarUsuario)
+
+
+menu4_1 = Menu(menuPrincipal, tearoff=0)
+
+menuPrincipal.add_cascade(label="Conexion", menu=menu4_1)
+menu4_1.add_command(label="Conexion")
+if hayInternet == False:
+    menu4_1.add_command(label="Internet", state = 'disable')
+else: 
+    menu4_1.add_command(label="Internet")
+
+EtiquetaNombre=Label(mainMenu, text="Nombre:" , bg="light gray", fg= "white")
+EtiquetaNombre.pack(padx=5, pady=5, ipadx=5, ipady=5)
+TxtNombre=Entry(mainMenu)
+TxtNombre.pack(fill=X,padx=5,pady=5,ipadx=5, ipady=5)
+
+EtiquetaDiametro=Label(mainMenu, text="Diametro:" , bg="light gray", fg= "white")
+EtiquetaDiametro.pack(padx=5, pady=5, ipadx=5, ipady=5)
+TxtDiametro=Entry(mainMenu)
+TxtDiametro.pack(fill=X,padx=5,pady=5,ipadx=5, ipady=5)
+
+EtiquetaEspesor=Label(mainMenu, text="Espesor:" , bg="light gray", fg= "white")
+EtiquetaEspesor.pack(padx=5, pady=5, ipadx=5, ipady=5)
+TxtEspesor=Entry(mainMenu)
+TxtEspesor.pack(fill=X,padx=5,pady=5,ipadx=5, ipady=5)
+
+EtiquetaLargo=Label(mainMenu, text="Largo:" , bg="light gray", fg= "white")
+EtiquetaLargo.pack(padx=5, pady=5, ipadx=5, ipady=5)
+TxtLargo=Entry(mainMenu)
+TxtLargo.pack(fill=X,padx=5,pady=5,ipadx=5, ipady=5)
+
+ButtonIniciar=Button(mainMenu,text="Iniciar", fg="blue", state = 'normal') 
+ButtonIniciar.pack(side=BOTTOM)
+
+ButtonStop=Button(mainMenu,text="Stop", fg="blue", state = 'normal')
+ButtonStop.pack(side=BOTTOM)
+
+ButtonArchivoNuevo=Button(mainMenu,text="Archivo Nuevo +", fg="blue", state = 'normal', command = guardarArchivo)
+ButtonArchivoNuevo.pack(side=BOTTOM)
 
 
 loginWindow.mainloop()
