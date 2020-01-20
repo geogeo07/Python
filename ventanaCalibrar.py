294584de11fa26987c1631614613ecba73f7a6ec
geovanni alvarez rivera <geovannitito9710@gmail.com>
2020-01-20 15:20:10 -0600

    Cambie por .place en vez de .grid

--- ventanaCalibrar.py
+++ ventanaCalibrar.py
@@ -7,25 +7,30 @@ from PIL import Image, ImageTk
 ##Cambiar el grid por place para tener posiciones absolutas..
 
 #Set up GUI
-window = tk.Tk()  #Makes main window
-window.wm_title("Digital Microscope")
-window.config(background="#FFFFFF")
-window.geometry("1000x600")
+ventanaCalibrar = tk.Tk()  #Makes main ventanaCalibrar
+ventanaCalibrar.wm_title("Calibracion manual")
+ventanaCalibrar.config(background="#FFFFFF")
+ventanaCalibrar.geometry("1000x600")
 
-#Graphics window
-imageFrame = tk.Frame(window, width=600, height=500)
-imageFrame.grid(row=0, column=0, padx=10, pady=2)
+#Graphics ventanaCalibrar
+#imageFrame = tk.Frame(ventanaCalibrar, width=600, height=500)
+#imageFrame.grid(row=0, column=0, padx=10, pady=2)
 
 #Capture video frames
-lmain = tk.Label(imageFrame)
-lmain.grid(row=0, column=0)
-cap = cv2.VideoCapture(0)
+labelVideo = tk.Label(ventanaCalibrar)
+labelVideo.place(x=10 , y=10)
+cap = cv2.VideoCapture(1)
 
 
 def show_frame():
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
-    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    #cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+
+
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
 
     cv2.circle(cv2image,(255,255), 50, (0,0,255), 1)
     cv2.line(cv2image,(200,255),(320,255),(0,255,0),2) #Horizontal
@@ -33,10 +38,6 @@ def show_frame():
 
 
     img = Image.fromarray(cv2image)
-
-    
-
-
     imgtk = ImageTk.PhotoImage(image=img)
     lmain.imgtk = imgtk
     lmain.configure(image=imgtk)
@@ -44,17 +45,17 @@ def show_frame():
 
 
 
-#Slider window (slider controls stage position)
-sliderFrame = tk.Frame(window, width=600, height=100)
-sliderFrame.grid(row = 800, column=0, padx=10, pady=2)
+#Slider ventanaCalibrar (slider controls stage position)
+#sliderFrame = tk.Frame(ventanaCalibrar, width=600, height=100)
+#sliderFrame.grid(row = 800, column=0, padx=10, pady=2)
 
 
-ButtonDerecha=tk.Button(window,text="Derecha", fg="blue", state = 'normal')
-ButtonDerecha.grid(row = 800, column=75, padx=5, pady=2)
+ButtonDerecha=tk.Button(ventanaCalibrar,text="Derecha", fg="blue", state = 'normal')
+ButtonDerecha.place(x=800, y=200)
 
-ButtonIzquierda=tk.Button(window,text="Izquierda", fg="blue", state = 'normal')
-ButtonIzquierda.grid(row = 800, column=50, padx=15, pady=2)
+ButtonIzquierda=tk.Button(ventanaCalibrar,text="Izquierda", fg="blue", state = 'normal')
+ButtonIzquierda.place(x=800, y=200)
 
 
 show_frame()  #Display 2
-window.mainloop()  #Starts GUI
+ventanaCalibrar.mainloop()  #Starts GUI
